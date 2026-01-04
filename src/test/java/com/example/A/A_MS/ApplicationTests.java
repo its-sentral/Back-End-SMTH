1e34a9707a54680d11729bf75feae111a7820497
its-sentral <Chavit.sar20@gmail.com>
2026-01-04 13:05:26 +0700

    Initial commit

---
+++ src/test/java/com/example/A/A_MS/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.A.A_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
